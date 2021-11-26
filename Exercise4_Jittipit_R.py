b0283aa0f5cd735385da78c4ca670fcbaec37873
NANOnoza <94231588+NANOnoza@users.noreply.github.com>
2021-11-26 15:27:44 +0700

    Create Exercise4_Jittipit_R.py

---
+++ Exercise4_Jittipit_R.py
@@ -0,0 +1,9 @@
+Eng = 81.5
+Busi = 97.3
+ComSys = 87.9
+ComProgram = 98.9
+print("--- Your Score ---")
+print("Foundation English : คะแนนที่ได้",Eng)
+print("General Business : คะแนนที่ได้",Busi)
+print("Introduction to Computer Systems : คะแนนที่ได้",ComSys)
+print("Computer Programming : คะแนนที่ได้",ComProgram)
