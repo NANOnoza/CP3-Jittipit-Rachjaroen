25b1ea5fce926d27a224e5354738c20cc5072a90
NANOnoza <94231588+NANOnoza@users.noreply.github.com>
2021-11-30 21:08:14 +0700

    Create Exercise9_Jittipit_R.py

---
+++ Exercise9_Jittipit_R.py
@@ -0,0 +1,7 @@
+user = input("Username : ")
+password = input("Password :")
+while user != 'Nano' or password !='12312121':
+    print("Ty again")
+    user = input("Username : ")
+    password = input("Password :")
+print("Done!")
